40d6cfa24c5a65c1e126c753a01cbeaff9f9b1c2
ManikHossain08 <manik083017@gmail.com>
2020-09-16 11:00:46 -0400

    learend from chapter04 from the books

---
+++ Moden-Java-In-Action-Book/src/chapter04/HighCaloriesNames.java
@@ -0,0 +1,36 @@
+package chapter04;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+public class HighCaloriesNames {
+
+  public static void main(String[] args) {
+	  
+    List<String> names = Dish.menu.stream()
+        .filter(dish -> {
+          System.out.println("filtering " + dish.getName());
+          return dish.getCalories() > 300;
+        })
+        .map(dish -> {
+          System.out.println("mapping " + dish.getName());
+          return dish.getName();
+        })
+        .limit(3)
+        .collect(toList());
+    System.out.println(names);
+  
+    // I did this
+    List<String> name = Dish.menu.stream()
+    		.filter( dish-> {
+    			return dish.getCalories() > 100;
+    		}).
+    		map(dish -> {
+    			return dish.getName();
+    		}).
+    		collect(toList());
+    
+    System.out.print(name);
+  }
+}
